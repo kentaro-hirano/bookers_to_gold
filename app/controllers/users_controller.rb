61cee249ca4c637b0c42cfdafc383bf45e8254c1
vagrant <vagrant@localhost.localdomain>
2020-08-26 13:46:23 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = User.new
+    @users = User.all
+    @book = Book.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    if current_user.id != @user.id
+      redirect_to user_path(current_user)
+    end
+  end
+
+ 
+
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] = "User was successfully updated."
+    redirect_to user_path(@user)
+    else
+      @users = User.all  
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+   params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  
+
+
+end
+
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '9359f596b14b19122690864c7e5f155b169cd927c3612962aeaf6786eb6fa076a065767c098b79ee5c40081f76b3bc6109c151c7d347003e923f42e664ba982d'
